0f23653f6cf89d5ecdfcf42a8f6a3557675f63b7
Tini Cassi <89404026+agustinacassi@users.noreply.github.com>
2023-07-17 18:06:12 -0300

    Authentication modal and functionality

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft } from "react-icons/rx";
@@ -6,6 +7,10 @@ import { RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -13,10 +18,21 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const authModal = useAuthModal();
   const router = useRouter();
 
-  const handleLogout = () => {
-    //Handles logout in the future
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    //TODO: reset any playing songs.
+    router.refresh();
+    if (error) {
+      toast.error(error.message);
+    }else{
+      toast.success('Logged out!')
+    }
   };
 
   return (
@@ -31,15 +47,15 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         className
       )}
     >
-      <div className="w-full md-4 flex items-center justify-between">
+      <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
           <button
             onClick={() => router.back()}
-            className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 cursor-pointer transition"
           >
             <RxCaretLeft size={35} className="text-white" />
           </button>
-          <button className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+          <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 cursor-pointer transition">
             <RxCaretRight
               onClick={() => router.forward()}
               size={35}
@@ -56,21 +72,38 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
-          <>
-            <div>
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
               <Button
-                onClick={() => {}}
-                className="bg-transparent text-neutral-300 font-medium"
+                onClick={() => router.push("/account")}
+                className="bg-white"
               >
-                Sign Up
+                <FaUserAlt />
               </Button>
             </div>
-            <div>
-              <Button onClick={() => {}} className="bg-white px-6 py-2">
-                Log In
-              </Button>
-            </div>
-          </>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-transparent text-neutral-300 font-medium"
+                >
+                  Sign Up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-white px-6 py-2"
+                >
+                  Sign In
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
